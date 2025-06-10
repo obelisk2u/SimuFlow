4f0820d59168991c5320e314cf71338d5113ab24
obelisk2u <jdstout@gmail.com>
2025-06-10 15:47:58 -0400

    refactor: remove redundant field reassignments in boundary conditions

--- solver/boundary.py
+++ solver/boundary.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 def apply_boundary_conditions(fields, config, lid_velocity=1.0):
     u = fields["u"]
     v = fields["v"]
@@ -7,30 +5,17 @@ def apply_boundary_conditions(fields, config, lid_velocity=1.0):
 
     Ny, Nx = u.shape
 
-    # Velocity boundary conditions (u, v)
+    u[:, 0] = 0      # Left
+    u[:, -1] = 0     # Right
+    u[0, :] = 0      # Bottom
+    u[-1, :] = lid_velocity  # Top
 
-    # Left wall (x = 0): u = 0, v = 0
-    u[:, 0] = 0
     v[:, 0] = 0
-
-    # Right wall (x = Lx): u = 0, v = 0
-    u[:, -1] = 0
     v[:, -1] = 0
-
-    # Bottom wall (y = 0): u = 0, v = 0
-    u[0, :] = 0
     v[0, :] = 0
-
-    # Top wall (y = Ly): u = lid_velocity, v = 0
-    u[-1, :] = lid_velocity
     v[-1, :] = 0
 
-    # Pressure boundary condition (Neumann: dp/dn = 0)
-    p[:, 0] = p[:, 1]         # Left
-    p[:, -1] = p[:, -2]       # Right
-    p[0, :] = p[1, :]         # Bottom
-    p[-1, :] = p[-2, :]       # Top
-
-    fields["u"] = u
-    fields["v"] = v
-    fields["p"] = p
+    p[:, 0] = p[:, 1]
+    p[:, -1] = p[:, -2]
+    p[0, :] = p[1, :]
+    p[-1, :] = p[-2, :]
